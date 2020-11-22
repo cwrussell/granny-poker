1bbf2927add59d351de42e037db4f935d4ee48d0
Colin Russell <c-russell@live.com>
2020-11-22 13:46:42 -0700

    Suits are capitalized; the default set of face values is set in Deck

--- card.py
+++ card.py
@@ -1,30 +1,28 @@
-class CardError(Exceptions): pass
+class CardError(Exception): pass
 
 class Card:
 
-    def __init__(self, suit, face, value=None):
+    def __init__(self, suit, face, value):
         self._suit = self._validate_suit(suit)
         self._face = self._validate_face(face)
         self._value = self._set_value(value)
 
     def _validate_face(self, face):
         face = str(face).upper()
-        valid_faces = list(range(2, 11)) + ["K", "Q", "J", "A"] 
+        valid_faces = [str(x) for x in range(2, 11)] + ["K", "Q", "J", "A"] 
         if face not in valid_faces:
             raise CardError(f"Invalid face: {face}")
         return face
 
     def _validate_suit(self, suit):
-        suit = str(suit).lower()
-        if suit not in {"clubs", "spades", "diamonds", "hearts"}:
+        if suit not in {"Clubs", "Spades", "Diamonds", "Hearts"}:
             raise CardError(f"Invalid suit: {suit}")
         return suit
 
     def _set_value(self, value):
-        if value is None:
-            faces = ["A"] + list(range(2, 11)) + ["J", "Q", "K"]
-            def_values = {face: i + 1 for i, face in enumerate(faces)}
-            return def_values[self._face]
         if type(value) != int:
             raise CardError("Card value must be int")
         return value
+        
+    def __str__(self):
+        return f"{self._face} of {self._suit}"
--- deck.py
+++ deck.py
@@ -1,27 +1,51 @@
 from collections import deque
 from random import shuffle
 
-from .card import Card
+from card import Card
 
 class Deck:
 
-    def __init__(self):
+    def __init__(self, values={}):
+        self._values = values
         card_list = self._create_deck()
-        shuffled_cards = shuffle(card_list)
-        self._cards = deque(shuffled_cards)
+        shuffle(card_list)
+        self._cards = deque(card_list)
+        
+    DEFAULT_VALUES = {
+        "A": 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9,
+        10: 10, "J": 10, "Q": 10, "K": 10
+    }
+    
+    def _get_default_values(self):
+        faces = ["A"] + list(range(2, 11)) + ["J", "Q", "K"]
+        def_values = {face: i + 1 for i, face in enumerate(faces)}
+        return def_values
+        
+    def _get_value(self, face):
+        if face in self._values: return self._values[face]
+        else: return self.DEFAULT_VALUES[face]
 
     def _create_deck(self):
         cards = []
-        for suit in ["clubs", "spades", "diamonds", "hearts"]:
+        for suit in ["Clubs", "Spades", "Diamonds", "Hearts"]:
             for face in list(range(2, 11)):
-                card = Card(suit, face, face)
+                card = Card(suit, face, self._get_value(face))
                 cards.append(card)
-            cards.append(Card(suit, "A", 1))
+            cards.append(Card(suit, "A", self._get_value("A")))
             for face in ["J", "Q", "K"]:
-                cards.append(Card(suit, face, 10))
+                cards.append(Card(suit, face, self._get_value(face)))
+        return cards
 
     def next_card(self):
         return self._cards.popleft()
 
     def add_to_deck(self, card):
         self._cards.append()
+        
+    def deal(self, n_hands, n_cards):
+        hands = [[] for i in range(n_hands)]
+        for i in range(n_cards):
+            for j in range(n_hands):
+                hands[j].append(self.next_card())
+        return hands
+
